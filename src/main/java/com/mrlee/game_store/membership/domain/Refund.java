ee1dc94c77873b5a7b6feed8c12e905a9230f829
mrlee <lkt900520@gmail.com>
2025-03-14 12:45:36 +0900

    feat: Refund 엔티티, 리포지토리 추가

---
+++ src/main/java/com/mrlee/game_store/membership/domain/Refund.java
@@ -0,0 +1,33 @@
+package com.mrlee.game_store.membership.domain;
+
+import com.mrlee.game_store.common.AuditingDateTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "refund")
+@Entity
+public class Refund extends AuditingDateTime {
+
+    public enum RefundStatus {COMPLETE, FAILED}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refund_id")
+    private Long id;
+    @Column(name = "payment_id", nullable = false)
+    private Long paymentId;
+    @Column(name = "amount")
+    private int amount;
+    @Column(name = "reason", nullable = false)
+    private String reason;
+    @Column(name = "refund_date", nullable = false)
+    private LocalDateTime refundDate;
+    @Enumerated(EnumType.STRING)
+    private RefundStatus status;
+}
