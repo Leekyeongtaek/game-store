55bceba5315cf7e676f3aa903fbb506917b3491f
mrlee <lkt900520@gmail.com>
2025-03-25 20:52:34 +0900

    test: RestTemplate 연결 테스트 API 추가
    - /member-subscription/connection-test
    - /member-subscription/read-test

--- src/main/java/com/mrlee/game_store/membership/controller/MemberSubscriptionController.java
+++ src/main/java/com/mrlee/game_store/membership/controller/MemberSubscriptionController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberSubscriptionController {
 
     private final MemberSubscriptionService memberSubscriptionService;
+    private final RestTemplate restTemplate;
 
     @PostMapping("/join")
     public ResponseEntity<Void> joinMemberSubscription(@RequestBody MemberSubscriptionJoinForm form) {
@@ -31,4 +31,34 @@ public class MemberSubscriptionController {
         memberSubscriptionService.cancelMemberSubscription(form);
         return ResponseEntity.ok("멤버십 구독 취소 요청이 완료되었습니다.");
     }
+
+    /**
+     * RestTemplate 커넥션 타임아웃용 테스트 API
+     *
+     * @return
+     */
+    @GetMapping("/connection-test")
+    public ResponseEntity<Void> testConnection() {
+        try {
+            restTemplate.getForObject("http://10.255.255.1", String.class);
+        } catch (ResourceAccessException e) {
+            log.info("ConnectTimeout:: {}", e.getMessage());
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * HTTPBin HTTP 테스트용 API
+     * @return
+     */
+    @GetMapping("/read-test")
+    public ResponseEntity<Void> testReadTimeout() {
+        try {
+            restTemplate.getForObject("https://httpbin.org/delay/6", String.class);
+        } catch (ResourceAccessException e) {
+            log.info("ReadTimeout:: {}", e.getMessage());
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
